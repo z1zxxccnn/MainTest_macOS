4af90299caee43da699dfd5ae57ac3655d3edc27
z1zxxccnn <z.zxxccnn@163.com>
2018-02-01 22:10:54 +0800

    initialize project

---
+++ LibTest/LibTest.h
@@ -0,0 +1,19 @@
+//
+//  LibTest.h
+//  LibTest
+//
+//  Created by ztcd on 2018/2/1.
+//  Copyright © 2018年 ztcd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LibTest.
+FOUNDATION_EXPORT double LibTestVersionNumber;
+
+//! Project version string for LibTest.
+FOUNDATION_EXPORT const unsigned char LibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibTest/PublicHeader.h>
+
+
---
+++ MainTest_macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MainTest_macOS
+//
+//  Created by ztcd on 2018/2/1.
+//  Copyright © 2018年 ztcd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
